2188bf867783ddf35a2e06bd0447581be1580d38
mgemard <mathieu.gemard@gmail.com>
2015-11-06 21:49:26 +0100

    added victory detection

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 var count = 0;
+var current;
+var over = false;
+var position = [0,1,2,3,4,5,6,7,8];
 
-function myFunction(button){
+function play(num){
+console.log(over);
+  if(!over){
+    var elem = document.getElementById("button"+num);
+    var parent = elem.parentNode;
+    parent.removeChild(elem);
 
-  var elem = document.getElementById(button);
-  var parent = elem.parentNode;
-  parent.removeChild(elem);
-  if (count == 0) {
-  	parent.innerHTML = "X";
+    if (count == 0) {
+      current = "X";
+      position[num] = current;
+    	parent.innerHTML = current;
+      document.getElementById("note").innerHTML = "Next to play: O";
+    }
+    else {
+      current = "O";
+      position[num] = current;
+    	parent.innerHTML = current;
+      document.getElementById("note").innerHTML = "Next to play: X";
+    }
+    count = (count + 1 ) % 2;
+
+    over = isWinner();
+    if(over) {
+      document.getElementById("note").innerHTML = current + " Won";
+    }
   }
-  else {
-  	parent.innerHTML = "O";
-  }
-  count = (count + 1 ) % 2;
 
 }
 
@@ -23,17 +40,19 @@ function resetBoard(){
   }
 
   generateBoard();
+  over = false;
+  position = [0,1,2,3,4,5,6,7,8];
 }
 
 
 function generateBoard(){
   var plateau = document.createElement("table");
   var elem = "";
-  var num = 1;
+  var num = 0;
   for (var i = 1; i <= 3; i++){
     elem += "<tr>";
     for (var j = 1; j <= 3; j++){
-      elem += "<td><button id=\"button"+num+"\" onclick=\"myFunction('button"+num+"')\"></button></td>";
+      elem += "<td><button id=\"button"+num+"\" onclick=\"play('"+num+"')\"></button></td>";
       num++;
     }
     elem += "</tr>";
@@ -42,5 +61,14 @@ function generateBoard(){
 
   plateau.innerHTML = elem;
   document.getElementById("board").appendChild(plateau);
+  document.getElementById("note").innerHTML = "Next to play: X";
 
+}
+
+function isWinner(){console.log(position[0]+"   " +position[1] +"  "+ position[2]);
+  if(position[0] == position[1] && position[0] == position[2] ||
+     position[3] == position[4] && position[3] == position[5] ||
+     position[6] == position[7] && position[6] == position[8]) {
+       return true;
+     }
 }
