8cf511e2329387625734ae474c7854def0ee54b8
Mathieu Gemard <mathieu.gemard@gmail.com>
2015-11-06 16:49:13 +0100

    adding js function

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-function myFunction(){
+
+
+var count = 0;
+
+
+function myFunction(button){
 console.log("hi");
+
+var elem = document.getElementById(button1);
+    var parent = elem.parentNode;
+    parent.removeChild(elem);
+
+    if (count == 0) {
+    	parent.innerHTML = "X";
+    }
+    else {
+    	parent.innerHTML = "O";
+    }
+
+
+    count= count+1%2;
 
 }
